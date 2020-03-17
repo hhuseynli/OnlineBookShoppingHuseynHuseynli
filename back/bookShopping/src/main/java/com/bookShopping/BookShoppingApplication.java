86c18e6d27c4e2088173791739c28c2046011801
hhuseynli <huseyn00h1843@gmail.com>
2020-03-17 19:15:26 +0400

    File upload and download in backend

--- back/bookShopping/src/main/java/com/bookShopping/BookShoppingApplication.java
+++ back/bookShopping/src/main/java/com/bookShopping/BookShoppingApplication.java
@@ -1,13 +1,28 @@
 package com.bookShopping;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.bookShopping.storage.StorageProperties;
+import com.bookShopping.storage.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class BookShoppingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookShoppingApplication.class, args);
 	}
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+
+            storageService.init();
+        };
+    }
 
 }
---
+++ back/bookShopping/src/main/java/com/bookShopping/storage/StorageProperties.java
@@ -0,0 +1,14 @@
+package com.bookShopping.storage;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@ConfigurationProperties("storage")
+@Getter
+@Setter
+public class StorageProperties {
+
+	String location = "upload-dir";
+}
