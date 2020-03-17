838d4d381cba0bbe29ff5a44e73664d390ff96df
hhuseynli <huseyn00h1843@gmail.com>
2020-03-17 16:39:14 +0400

    Backend project created

---
+++ back/bookShopping/src/main/java/com/bookShopping/BookShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.bookShopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookShoppingApplication.class, args);
+	}
+
+}
---
+++ back/bookShopping/src/test/java/com/bookShopping/BookShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookShopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
