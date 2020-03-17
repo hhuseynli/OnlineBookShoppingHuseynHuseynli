d98e442be14aa7d51ac6cdfe1da394b27a8030f9
hhuseynli <huseyn00h1843@gmail.com>
2020-03-17 18:36:19 +0400

    security configuration

---
+++ back/bookShopping/src/main/java/com/bookShopping/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.bookShopping.config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        
+    			http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/filedownload/**").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .anyRequest().authenticated().and()
+                .httpBasic();
+
+
+    }  
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource);
+            
+    }
+
+
+ 
+ 
+}
