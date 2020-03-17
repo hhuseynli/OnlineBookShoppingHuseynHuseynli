55b258a5081ebe078c3e8d318da59227a74fa808
hhuseynli <huseyn00h1843@gmail.com>
2020-03-17 22:29:00 +0400

    SearchModel

---
+++ back/bookShopping/src/main/java/com/bookShopping/model/SearchModel.java
@@ -0,0 +1,13 @@
+package com.bookShopping.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SearchModel {
+	private String search;
+	private Integer begin;
+	private Integer length;
+
+}
