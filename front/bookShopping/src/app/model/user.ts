e13e49c31a00d48932aba5a7e75db07552f9dc4e
hhuseynli <huseyn00h1843@gmail.com>
2020-03-18 14:08:30 +0400

    user model

---
+++ front/bookShopping/src/app/model/user.ts
@@ -0,0 +1,8 @@
+export class User{
+    public username:string;
+    public password:string;
+    public enabled:number;
+    public name:string;
+    public surname:string;
+    public email:string;
+}
