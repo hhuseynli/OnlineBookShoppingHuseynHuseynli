9cd0af8a0e80b2b2f7c2b0024c886e02a7e8ead2
hhuseynli <huseyn00h1843@gmail.com>
2020-03-18 10:56:52 +0400

    solving incorrect property name problem

--- back/bookShopping/src/main/java/com/bookShopping/dao/BookDAO.java
+++ back/bookShopping/src/main/java/com/bookShopping/dao/BookDAO.java
@@ -12,7 +12,7 @@ public interface BookDAO extends JpaRepository<Book, Integer>{
 	@Query(value="select * from book where username=?1 limit ?2,?3", nativeQuery=true)
 	public List<Book> findByUsername(String username, Integer begin, Integer length);
 	
-	@Query(value="select * from book where name like %?1% limit ?2,?3", nativeQuery=true)
+	@Query(value="select * from book where book like %?1% limit ?2,?3", nativeQuery=true)
 	public List<Book> searchBook(String search, Integer begin, Integer length);
 	
 	@Query(value="select * from book where username not like ?1 limit ?2,?3", nativeQuery=true)
