8a17e8881b42fb4c7550fe70a8b0c7f420302c61
hhuseynli <huseyn00h1843@gmail.com>
2020-03-18 14:25:16 +0400

    Create user.service.ts

---
+++ front/bookShopping/src/app/service/user.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../model/user';
+import { API_URL } from '../constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http:HttpClient) {}
+
+  public logUser(): boolean {
+    let result:boolean= false;
+    if(sessionStorage.getItem("authorization")){
+      result=  true;
+    }
+
+    return result;
+  }
+
+  public createUser(user:User){
+    return this.http.post<User>(`${API_URL}/users`,user);
+  }
+}
