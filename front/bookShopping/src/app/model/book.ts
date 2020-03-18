d599c4d50aee57e95c2f5d19ac0190de1b64d961
hhuseynli <huseyn00h1843@gmail.com>
2020-03-18 13:59:11 +0400

    book model

---
+++ front/bookShopping/src/app/model/book.ts
@@ -0,0 +1,10 @@
+export class Book{
+    public id:number;
+	public book:string;
+	public description:string;
+	public image:string;
+	public price:number;
+	public author:string;
+	public page:number;
+	public username:string;
+}
