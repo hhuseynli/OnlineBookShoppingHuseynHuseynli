d722335d3bb25b7d498ae8fe203a39eb28772622
hhuseynli <huseyn00h1843@gmail.com>
2020-03-19 17:56:15 +0400

    upload file

---
+++ front/bookShopping/src/app/service/file.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ImageBean } from '../model/imageBean';
+import { API_URL } from '../constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileService {
+
+  constructor(private http:HttpClient) { }
+
+  uploadImage(file:FormData){
+    return this.http.post<ImageBean>(`${API_URL}/fileupload`, file);
+  }
+
+
+}
